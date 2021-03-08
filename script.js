ae5df802445a9d98c42e41502b4bd5ab7189022a
pdagri <pdagri@yahoo.fr>
2021-03-08 22:22:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function staticLoadPlaces() {
 var models = [
     {
         url: './assets/magnemite/scene.gltf',
-        scale: '0.05 0.05 0.05',
+        scale: '0.005 0.005 0.005',
         info: 'Magnemite, Lv. 5, HP 10/10',
         rotation: '0 180 0',
     },
